d674079b3b8462a6bea6a8a7f1a022dad6e06ea3
oauth2 <184812659+Velatryx@users.noreply.github.com>
2025-11-16 09:29:58 -0500

    w

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range (0, 10):
-    for n in range(i + 1, 10):
-        if i == 8 and n == 9:
-            print("{}{}".format(i,n))
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
         else:
-            print("{}{}".format(i,n), end=", ")
+            print("{}{}".format(i, j), end=", ")
