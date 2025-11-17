d769f7ca50005db68e7e5d3245b706780f958030
root <184812659+Velatryx@users.noreply.github.com>
2025-11-17 10:29:07 +0000

    lists

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#
+#
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
+
+if __name__ == "__main__":
+    my_list = [0, 1, 2, 3, 10]
+    print_list_integer(my_list)
+#
+#
+# custom_list = ['v', 'e', 'l', 'a', 't', 'r', 'y', 'x']
+#
+#
+# def print_list_integer(my_list=[]):
+#    return str.format(my_list)
+# if __name__ == "__main__":
+#    for i in range(0, len(custom_list)):
+#        print("{}".format(f"{custom_list[i]}", end="\n"))
