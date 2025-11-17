eab51e3fdd107751fc38944e853ddb92214b3126
root <184812659+Velatryx@users.noreply.github.com>
2025-11-17 09:57:16 +0000

    hidden sort

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import importlib.util
+
+if __name__ == "__main__":
+    spec = importlib.util.spec_from_file_location("hidden_4", "/tmp/hidden_4.pyc")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    names = [name for name in dir(module) if not name.startswith("__")]
+    names.sort()
+    for name in names:
+        print("{}".format(f"{name}"))
