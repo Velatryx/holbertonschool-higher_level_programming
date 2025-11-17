43f78df542f03d0ba49d4db1a7911ac456f875bf
root <184812659+Velatryx@users.noreply.github.com>
2025-11-17 13:24:03 +0000

    fix

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@
 def max_integer(my_list=[]):
     max = my_list[0]
     if not my_list:
-        return None
+        return print("Max: ", None)
     for i in my_list:
         if i > max:
             max = i
