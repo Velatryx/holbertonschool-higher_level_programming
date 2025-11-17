28f94e4f5aed76122ad0df22bcbe7a5d4447bbc7
root <184812659+Velatryx@users.noreply.github.com>
2025-11-17 09:59:48 +0000

    fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,9 @@
 import importlib.util
 
 if __name__ == "__main__":
-    spec = importlib.util.spec_from_file_location("hidden_4", "/tmp/hidden_4.pyc")
+    spec = importlib.util.spec_from_file_location(
+        "hidden_4", "/tmp/hidden_4.pyc"
+    )
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     names = [name for name in dir(module) if not name.startswith("__")]
