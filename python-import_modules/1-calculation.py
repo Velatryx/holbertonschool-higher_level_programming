bb17d255e31c0ad73a7c92b64e16960358ceca4d
root <184812659+Velatryx@users.noreply.github.com>
2025-11-17 08:05:47 +0000

    fix, calc is short for calculator btw for those who are new in the chat

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div  ##Normally use import *, but it is forbidden for this task.
+from calculator_1 import add, sub, mul, div
+# Normally use import *, but it is forbidden for this task.
 a = 10
 b = 5
 if __name__ == "__main__":
