4bddb70202191af83c3091bd8b0df9d7c6adac49
root <184812659+Velatryx@users.noreply.github.com>
2025-11-17 08:59:14 +0000

    args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    num = len(argv) - 1
+    if num == 0:
+        print("{}".format("0 arguments."))
+    elif num == 1:
+        print("{}".format("1 argument."))
+        print("{}".format(f"1: {argv[1]}"))
+    else:
+        print("{}".format(f"{num} arguments."))
+        for i in (1, num):
+            print("{}".format(f"{i}: {argv[i]}", end="\n"))
