4a72f7b448b4d76d788d27c2852e12d212b9b646
root <184812659+Velatryx@users.noreply.github.com>
2025-11-18 11:13:50 +0000

    fix

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,9 @@
 
 def search_replace(my_list, search, replace):
     new_list = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            new_list[i] = replace
+        for j in my_list:
+            if j == search:
+                new_list.append(replace)
+            else:
+                new_list.append(j)
     return new_list
