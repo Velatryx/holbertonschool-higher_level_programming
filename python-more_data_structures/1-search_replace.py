dcc5c25da66f33a5b46ed64a30325e455f486877
root <184812659+Velatryx@users.noreply.github.com>
2025-11-18 10:57:56 +0000

    fix

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -6,9 +6,4 @@ def search_replace(my_list, search, replace):
     for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
-    return new_list
-
-
-if __name__ == "__main__":
-    my_list = [0, 1, 2, 3, 4, 2, 2, 2, 3, 1, 90]
-    print(search_replace(my_list, 2, 10))
+    return print(f"{my_list}\n{new_list}")
