d5bcd98ec65c11855102a0f61e6022d94169d5ce
root <184812659+Velatryx@users.noreply.github.com>
2025-11-21 12:30:21 +0000

    best

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    for key, value in a_dictionary.items():
+        best = None
+        if value > best:
+            best = value
+            best_key = key
+    return best_key
