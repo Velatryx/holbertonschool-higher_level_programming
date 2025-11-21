c76f4a21fb000103bbb2ecf010b1be1a26412f01
root <184812659+Velatryx@users.noreply.github.com>
2025-11-21 12:47:08 +0000

    fix

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,17 +2,13 @@
 
 
 def best_score(a_dictionary):
-    best = next(iter(a_dictionary.values()))
-    best_key = None
     if not a_dictionary:
         return None
+    best_key = next(iter(a_dictionary))
+    best_value = a_dictionary[best_key]
     for key, value in a_dictionary.items():
-        if value > best:
-            best = value
+        if value > best_value:
+            best_value = value
             best_key = key
+
     return best_key
-
-
-if __name__ == "__main__":
-    a_dictionary = {'Hello': 12, 'Bye': 14, 'Dummy': 10}
-    print(best_score(a_dictionary))
