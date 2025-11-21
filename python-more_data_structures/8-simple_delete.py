e9222f68e51487c85ceccc72040a9b9354e4357d
root <184812659+Velatryx@users.noreply.github.com>
2025-11-21 12:04:51 +0000

    fi

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton3
+
+
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
+
+
+# a_dictionary.pop(key, None)
