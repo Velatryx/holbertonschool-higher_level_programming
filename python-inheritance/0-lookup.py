20a1f039a0309367bd5b2052b12fc72846b5abe7
Murx <184812659+Velatryx@users.noreply.github.com>
2025-11-30 14:43:48 +0400

    Update 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Defines a function that returns the list of available attributes and methods of an object."""
-    
+"""Defines a function that returns the list of available attributes
+and methods of an object.
+"""
+
 
 def lookup(obj):
     """Return a list of attributes and methods of the given object."""
