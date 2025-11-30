66331eecfd20546da7b1d5e7bbf0d726ee7944f0
Murx <184812659+Velatryx@users.noreply.github.com>
2025-11-30 14:50:50 +0400

    Update 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines a function that checks object class equality."""
-    
+
 
 def is_same_class(obj, a_class):
     """True if obj is exactly an instance of a_class,otherwise False"""
