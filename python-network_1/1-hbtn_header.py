fc22a195db468ff10ceb36b499e40cc58e0cbcfc
oauth2 <184812659+Velatryx@users.noreply.github.com>
2025-12-07 09:15:45 -0500

    f

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Fetches the X-Request-Id header from a given URL.
+"""
+
 import urllib.request
 import sys
 
@@ -6,5 +10,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        headers = response.headers
-        print(headers.get("X-Request-Id"))
+        print(response.headers.get("X-Request-Id"))
